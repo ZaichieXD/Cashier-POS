f54ca70aaae9dcaba028420be3a08f21b6209b9c
ZaichieXD <charleszolina19@gmail.com>
2023-05-16 18:08:20 +0800

    #2 Added Data Validation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,51 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let validatedUsername = "";
+let validatedPassword = "";
 
-// Write your JavaScript code.
+// These functions are for the login page
+{
+    // Logins the user if the data is valid
+    function userLogin() {
+        var username = document.getElementById("username-text").value;
+        var password = document.getElementById("password-text").value;
+        if (username == "" || password == "") {
+            alert("Please enter username and password");
+        }
+        else {
+            validateUserName(username);
+            validatePassword(password);
+        }
+
+        clearFields();
+    }
+
+    // Deletes the data in the fields
+    function clearFields() {
+        document.getElementById("username-text").value = "";
+        document.getElementById("password-text").value = "";
+    }
+
+    // Validates the username and makes sure it has POS
+    function validateUserName(username) {
+        if (username.includes("POS")) {
+            validatedUsername = username;
+        }
+        else {
+            alert("Invalid credentials");
+        }
+
+        console.log("User Validated");
+    }
+
+    // Validates the password and makes sure it has 090
+    function validatePassword(password) {
+        if (password.length > 8 && password.includes("090")) {
+            validatedPassword = password;
+
+        }
+        else {
+            alert("Invalid credentials");
+        }
+
+        console.log("User Validated");
+    }
+}
